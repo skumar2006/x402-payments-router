e5f8db18b370517bff59ab0addc612a31515dcb4
Shivam Kumar <shivamkumar@Shivams-MacBook-Pro.local>
2025-11-22 05:28:25 -0300

    build Ready

--- paymentSystem/next.config.js
+++ paymentSystem/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Suppress warnings for optional dependencies
+    config.ignoreWarnings = [
+      { module: /@react-native-async-storage/ },
+    ];
+    return config;
+  },
 }
 
 module.exports = nextConfig
